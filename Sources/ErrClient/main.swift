3caeb22b7706fc9bc86cfb2fa6c29cd1dc2f5750
Walter Scott <w@lter.ca>
2024-08-09 14:44:23 -0400

    add more examples

--- Sources/ErrClient/main.swift
+++ Sources/ErrClient/main.swift
@@ -5,15 +5,27 @@ func myThrowingFunc(_ arg: Int) throws -> UInt32 {
 	return UInt32(arg)
 }
 
+func myThrowingAsyncFunc(_ arg: Int) async throws -> UInt32 {
+	return UInt32(arg)
+}
+
 struct Hello: Error {}
 
 @err
-func checker() -> Result<String, Error> {
+func checker() async -> Result<String, Error> {
 	guard let res2 = try myThrowingFunc(12) else {
 		return .failure(err)
 	}
 
 	guard let res3 = Result(catching: { try myThrowingFunc(12) }).err() else {
+		return .failure(err)
+	}
+	
+	guard let res4 = try myThrowingFunc(12) else {
+		return .failure(err)
+	}
+	
+	guard let res5 = try await  myThrowingAsyncFunc(12) else {
 		return .failure(err)
 	}
 

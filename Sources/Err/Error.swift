024e1406519662a97d58bb92b5eae64ebd7b8e15
Walter Scott <w@lter.ca>
2025-07-28 16:45:46 -0500

    fix: nil cause return

--- Sources/Err/Error.swift
+++ Sources/Err/Error.swift
@@ -3,7 +3,7 @@ extension Swift.Error {
 	public func `cause`<T: Error>(as type: T.Type) -> T? {
 		// deepest error of type T
 		guard let err = self as? WithCause else {
-			return self as? T
+			return nil
 		}
 
 		return err.deepest(ofType: T.self)
@@ -11,7 +11,7 @@ extension Swift.Error {
 
 	public func cause() -> Error? {
 		guard let err = self as? WithCause else {
-			return self
+			return nil
 		}
 
 		return err.deepest(ofType: Error.self)
